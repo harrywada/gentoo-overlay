2937cd84001bb91efa3eda0b8c7f84ca20dae3e4
Harry Wada <harrywada@gmail.com>
2022-06-04 20:42:02 -0500

    Change the OpenRC script for www-apps/xandikos to allow configuration.
    Have the command_args variable use the existing value if it is set, and
    use the default otherwise. This allows command_args to be overridden in
    an /etc/conf.d/xandikos file.

--- www-apps/xandikos/files/xandikos-0.2.8.init.d
+++ www-apps/xandikos/files/xandikos-0.2.8.init.d
@@ -3,7 +3,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 command="/usr/bin/xandikos"
-command_args="-d /var/lib/xandikos"
+command_args="${command_args:--d /var/lib/xandikos}"
 command_background=true
 command_user="xandikos:xandikos"
 pidfile="/run/${RC_SVCNAME}.pid"
